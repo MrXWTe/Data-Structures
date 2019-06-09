ed6daf3447c8338dba697d8e5c8384f4cafc0782
MrXWTe <WeitengXu@163.com>
2019-06-09 23:33:34 +0800

    finish BST

--- BST/src/Main.java
+++ BST/src/Main.java
@@ -2,5 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        BST<Integer> bst = new BST<>();
+
+        int[] nums = {2,5,1,9,6,3};
+
+        for(int i : nums){
+            bst.add(i);
+        }
+
+        bst.preOrder();
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+        bst.postOrder();
+        //System.out.println(bst.toString());
     }
 }
